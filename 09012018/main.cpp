05e074a93e95515917a8a5dd167b89114ed59d61
Andrea Leang <andrea@Andreas-MacBook-Air.local>
2018-09-01 20:36:13 -0700

    Initial Commit

---
+++ 09012018/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  09012018
+//
+//  Created by Andrea Leang on 9/1/18.
+//  Copyright © 2018 Andrea Leang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
